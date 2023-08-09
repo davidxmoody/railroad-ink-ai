54918f8f918927bd3a8f1ce8ce57aa1b96a3e11e
David Moody <david@davidxmoody.com>
2023-08-09 11:30:52 +0100

    Add dice

---
+++ src/dice.ts
@@ -0,0 +1,33 @@
+export type TrackType = "d" | "l"
+
+export interface Tile {
+  0?: TrackType // North
+  1?: TrackType // East
+  2?: TrackType // South
+  3?: TrackType // West
+  overpass?: boolean
+}
+
+export const routeDieA: Tile[] = [
+  {1: "d", 3: "d"},
+  {1: "l", 3: "l"},
+  {1: "d", 2: "d"},
+  {1: "l", 2: "l"},
+  {1: "d", 2: "d", 3: "d"},
+  {1: "l", 2: "l", 3: "l"},
+]
+
+export const routeDieB: Tile[] = [
+  {0: "d", 1: "l", 2: "d", 3: "l", overpass: true},
+  {1: "d", 3: "l"},
+  {2: "d", 3: "l"},
+]
+
+export const specialRouteTiles: Tile[] = [
+  {0: "d", 1: "d", 2: "l", 3: "d"},
+  {0: "d", 1: "l", 2: "l", 3: "l"},
+  {0: "d", 1: "d", 2: "d", 3: "d"},
+  {0: "l", 1: "l", 2: "l", 3: "l"},
+  {0: "d", 1: "l", 2: "l", 3: "d"},
+  {0: "d", 1: "l", 2: "d", 3: "l"},
+]
