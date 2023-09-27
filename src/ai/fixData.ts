7fc9cc8f0e5d8d73d4913b2dba53af2efd5f1f5a
David Moody <david@davidxmoody.com>
2023-09-27 08:53:27 +0100

    Allow data fixing script to skip existing records

--- src/ai/fixData.ts
+++ src/ai/fixData.ts
@@ -5,15 +5,26 @@ import readJsonl from "./readJsonl"
 import calculateScore from "../logic/calculateScore"
 import exhaustiveSearch from "./exhaustiveSearch"
 
-const gameRecords = readJsonl<GameRecord>("./src/data/training.jsonl")
+const originalGameRecords = readJsonl<GameRecord>("./src/data/training.jsonl")
 
-gameRecords.sort((a, b) => {
+const newGameRecordSeeds = readJsonl<GameRecord>(
+  "./src/data/trainingFixed.jsonl",
+).reduce(
+  (acc, ngr) => ({...acc, [ngr.seed]: true}),
+  {} as Record<string, boolean>,
+)
+
+originalGameRecords.sort((a, b) => {
   const aSeed = parseInt(a.seed.replace("training-", ""), 10)
   const bSeed = parseInt(b.seed.replace("training-", ""), 10)
   return aSeed - bSeed
 })
 
-for (const game of gameRecords) {
+for (const game of originalGameRecords) {
+  if (newGameRecordSeeds[game.seed]) {
+    continue
+  }
+
   const gameTiles = rollGameDice(game.seed)
   let gs = new GameState(undefined, gameTiles[0])
   while (gs.roundNumber <= 6) {
@@ -25,8 +36,8 @@ for (const game of gameRecords) {
   gs = gs.makeMoves(finalMoves)
 
   const score = calculateScore(gs.board).total
-  if (score !== game.score)
-    throw new Error(`Score mismatch ${score}, ${game.score}`)
+  if (score < game.score)
+    throw new Error(`New score lower than old score ${score}, ${game.score}`)
 
   const newGame: GameRecord = {
     seed: game.seed,
