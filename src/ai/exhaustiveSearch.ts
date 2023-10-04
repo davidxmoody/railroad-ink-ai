2512c3cafafdc8f769071a471028af7c6b31e77a
David Moody <david@davidxmoody.com>
2023-10-04 15:35:39 +0100

    Tidy up exhaustive search experiments

--- src/ai/exhaustiveSearch.ts
+++ src/ai/exhaustiveSearch.ts
@@ -1,45 +1,20 @@
 import type GameState from "../logic/GameState"
 import calculateScore from "../logic/calculateScore"
 import getMeaningfulPlacements from "../logic/getMeaningfulPlacements"
-import {isSpecial} from "../logic/helpers"
-import type {TileString} from "../logic/types"
-
-// { total: 300, neverUsedSpecial: 7, couldUseNoSpecial: 28 }
-
-const stats = {
-  total: 0,
-  neverUsedSpecial: 0,
-  couldUseNoSpecial: 0,
-}
 
 export default function exhaustiveSearch(gs: GameState) {
-  let bestMoves: string[][] = []
+  let bestMoves: string[] = []
   let bestScore = -Infinity
 
   for (const [endGs, endMoves] of visitAllStates(gs, [], new Set())) {
     const endScore = calculateScore(endGs.board).total
     if (endScore > bestScore) {
       bestScore = endScore
-      bestMoves = [endMoves]
-    } else if (endScore === bestScore) {
-      bestMoves.push(endMoves)
+      bestMoves = endMoves
     }
   }
 
-  const specialUsage = bestMoves.map((moves) =>
-    moves.some((move) => isSpecial(move.slice(2) as TileString)),
-  )
-
-  const neverUsedSpecial = specialUsage.every((x) => !x)
-  const couldUseNoSpecial = specialUsage.some((x) => !x)
-
-  stats.total++
-  if (neverUsedSpecial) stats.neverUsedSpecial++
-  if (couldUseNoSpecial) stats.couldUseNoSpecial++
-
-  console.log(stats)
-
-  return bestMoves[0]
+  return bestMoves
 }
 
 function* visitAllStates(
